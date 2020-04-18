e09d60a12e1e037bfe31236b99b4c63712d83219
Cees <cjol99@icloud.com>
2020-04-18 13:55:09 +0200

    Code cleanup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-console.log(firebaseConfig);
-
 const database = firebase.database();
 
 export { firebase, database as default };
